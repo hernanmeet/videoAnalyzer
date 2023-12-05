4fb40452be27cef9956317865c34c4d4cb64bba0
Hernan H. <hernanlitio@gmail.com>
2023-12-05 13:54:15 -0300

    Se cambio en orquestador.py las funciones de hilo por funciones con asyncio

--- orquestador.py
+++ orquestador.py
@@ -1,3 +1,4 @@
+import asyncio
 import threading
 from audio import captar_audio
 from logos import captar_logos
@@ -13,84 +14,81 @@ from Report_generator import generar_informe
 import time
 import redis
 
-
-
 redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
 
-
 lista = []
-ocupado=False
-def procesar(archivo):
-   global ocupado
-   
-   path_google = archivo.get('path_google')
-   
-   if (archivo.get('audio')=='true'):
-      captar_audio(path_google)
+ocupado = False
 
-   if (archivo.get('logos')=='true'):
-      captar_logos(path_google)
+async def procesar(archivo):
+    global ocupado
 
-   if (archivo.get('etiquetas')=='true'):
-      captar_etiquetas(path_google)
+    path_google = archivo.get('path_google')
 
-   if (archivo.get('personas')=='true'):   
-      captar_personas(path_google)
-   
-   if (archivo.get('textos')=='true'): 
-      captar_textos(path_google)
+    if archivo.get('audio') == 'true':
+        captar_audio(path_google)
 
-   if (archivo.get('textospeech')=='true'): 
-      sonidos(path_google)
+    if archivo.get('logos') == 'true':
+        captar_logos(path_google)
 
-   if (archivo.get('imagen_etiquetas')=='true'):
-      captar_imagen_etiquetas(path_google)
+    if archivo.get('etiquetas') == 'true':
+        captar_etiquetas(path_google)
 
-   if (archivo.get('imagen_landmark')=='true'):
-      captar_imagen_landmark(path_google)
-   
-   if (archivo.get('imagen_caras')=='true'):
-      captar_imagen_caras(path_google)
-   
-   if (archivo.get('imagen_textos')=='true'):
-      captar_imagen_textos(path_google)
-   
-   generar_informe(path_google)
-   
-   print("---------- FIN DEL ANALISIS -----------")
-   ocupado=False
+    if archivo.get('personas') == 'true':
+        captar_personas(path_google)
 
-print ("iniciando programa")
-while True:
-    estado = redis_client.get('TERMINO').decode('utf-8')
-    #print("Estado: ", redis_client.get('SE_TERMINO').decode('utf-8') )
-    time.sleep(1)
-    if (estado=='True'):
-       print("---------------- Iniciando Analisis -----------------")
-       
-       #datos = ({'path_google':redis_client.get('PATH_GOOGLE').decode('utf-8'),'path_local':redis_client.get('PATH_LOCAL').decode('utf-8')})
-       datos = ({
-                  'path_google':redis_client.get('PATH_GOOGLE').decode('utf-8'),
-                  'etiquetas':redis_client.get('ETIQUETAS').decode('utf-8'),
-                  'logos':redis_client.get('LOGOS').decode('utf-8'),
-                  'personas':redis_client.get('PERSONAS').decode('utf-8'),
-                  'textos':redis_client.get('TEXTOS').decode('utf-8'),
-                  'audio':redis_client.get('AUDIO').decode('utf-8'),
-                  'textospeech':redis_client.get('TEXTOSPEECH').decode('utf-8'),
-                  'imagen_etiquetas':redis_client.get('IMAGEN_ETIQUETAS').decode('utf-8'),
-                  'imagen_landmark':redis_client.get('IMAGEN_LANDMARK').decode('utf-8'),
-                  'imagen_caras':redis_client.get('IMAGEN_CARAS').decode('utf-8'),
-                  'imagen_textos':redis_client.get('IMAGEN_TEXTOS').decode('utf-8'),
- 
+    if archivo.get('textos') == 'true':
+        captar_textos(path_google)
 
-                 
-                 })
-       lista.append(datos)
-       redis_client.set('TERMINO', 'False')
-    
-    if (len(lista)>0 and not ocupado):
-        finalizado=True
-        #mi_hilo = threading.Thread(target= procesar(lista[0])
-        mi_hilo = threading.Thread(target=procesar, args=(lista[0],))
-        mi_hilo.start() 
-        lista.pop(0)
+    if archivo.get('textospeech') == 'true':
+        sonidos(path_google)
+
+    if archivo.get('imagen_etiquetas') == 'true':
+        captar_imagen_etiquetas(path_google)
+
+    if archivo.get('imagen_landmark') == 'true':
+        captar_imagen_landmark(path_google)
+
+    if archivo.get('imagen_caras') == 'true':
+        captar_imagen_caras(path_google)
+
+    if archivo.get('imagen_textos') == 'true':
+        captar_imagen_textos(path_google)
+
+    generar_informe(path_google)
+
+    print("---------- FIN DEL ANALISIS -----------")
+    ocupado = False
+
+async def procesar_archivos():
+    global lista, ocupado
+
+    while True:
+        estado = redis_client.get('TERMINO').decode('utf-8')
+        time.sleep(1)
+
+        if estado == 'True':
+            print("---------------- Iniciando Analisis -----------------")
+            datos = {
+                'path_google': redis_client.get('PATH_GOOGLE').decode('utf-8'),
+                'etiquetas': redis_client.get('ETIQUETAS').decode('utf-8'),
+                'logos': redis_client.get('LOGOS').decode('utf-8'),
+                'personas': redis_client.get('PERSONAS').decode('utf-8'),
+                'textos': redis_client.get('TEXTOS').decode('utf-8'),
+                'audio': redis_client.get('AUDIO').decode('utf-8'),
+                'textospeech': redis_client.get('TEXTOSPEECH').decode('utf-8'),
+                'imagen_etiquetas': redis_client.get('IMAGEN_ETIQUETAS').decode('utf-8'),
+                'imagen_landmark': redis_client.get('IMAGEN_LANDMARK').decode('utf-8'),
+                'imagen_caras': redis_client.get('IMAGEN_CARAS').decode('utf-8'),
+                'imagen_textos': redis_client.get('IMAGEN_TEXTOS').decode('utf-8'),
+            }
+
+            lista.append(datos)
+            redis_client.set('TERMINO', 'False')
+
+        if lista and not ocupado:
+            ocupado = True
+            await asyncio.gather(procesar(lista[0]))
+            lista.pop(0)
+
+if __name__ == "__main__":
+    asyncio.run(procesar_archivos())
